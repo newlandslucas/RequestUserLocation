e3954c4c52f2ef008c149d8cbbe5b9a0719ab180
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-13 14:42:19 -0300

    initial commit

---
+++ RequestUserLocation/LocationRequestView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationRequestView.swift
+//  RequestUserLocation
+//
+//  Created by Lucas Newlands on 13/04/22.
+//
+
+import SwiftUI
+
+struct LocationRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationRequestView()
+    }
+}
---
+++ RequestUserLocation/RequestUserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestUserLocationApp.swift
+//  RequestUserLocation
+//
+//  Created by Lucas Newlands on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RequestUserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
